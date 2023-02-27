8b5e21f9703087d7b274f4ba066f5e20d6da8e83
cassiano-dio <cassiano@dio.me>
2023-02-27 16:44:23 -0300

    Primeiro Commit

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+const AWS = require('aws-sdk')
+
+const ENDPOINT = 'cxgi078p19.execute-api.us-east-1.amazonaws.com/production/'
+const client = new AWS.ApiGatewayManagementApi({endpoint: ENDPOINT})
+const names = {};
+
 const { Configuration, OpenAIApi } = require("openai");
 require('dotenv').config()
-const express = require("express");
-
-const app = express();
-app.use(express.json());
 
 const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
@@ -27,14 +29,80 @@ async function runCompletion(message) {
     return completion.data.choices[0].text
 }
 
+const sendToBot = async (ids, mess) => {
+    let res = await runCompletion(mess);
+    let message = {publicMessage: `Bot : ${res}`}
+    const all = ids.map(i => sendToOne(i, message));
+    return Promise.all(all);
+};
 
-app.post("/message", async (req, res) => {
-    let message = await runCompletion(req.body.message);
-    let response = JSON.stringify({mesage: message})
-    res.send(response);
-});
+const sendToOne = async (id, body) => {
+    try {
+        await client.postToConnection({
+            'ConnectionId': id,
+            'Data':Buffer.from(JSON.stringify(body))
+        }).promise();
+    } catch (e) {
+        console.log(e);
+    }
+};
 
+const sendToAll = async (ids, body) => {
+    console.log("BODY:", body)
+    const all = ids.map(i => sendToOne(i, body));
+    return Promise.all(all);
+};
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listening on port ${port}...`));
-
+exports.handler = async(event) => {
+    
+    if(event.requestContext){
+        const connectionId = event.requestContext.connectionId;
+        const routeKey = event.requestContext.routeKey;
+        let body = {};
+        try{
+            if(event.body){
+                body = JSON.parse(event.body);
+            }
+        }catch (err){
+            console.log(err);
+        }
+        
+        switch(routeKey){
+            case '$connect':
+                break;
+            case '$disconnect':
+                await sendToAll(Object.keys(names), {systemMessage: `${names[connectionId]} acabou de sair.`});
+                delete names[connectionId];
+                await sendToAll(Object.keys(names), {members:Object.values(names)});
+                break;
+            case '$default':
+                break;
+            case 'setName':
+                names[connectionId] = body.name;
+                await sendToAll(Object.keys(names), {members:Object.values(names)});
+                await sendToAll(Object.keys(names), {systemMessage: `${names[connectionId]} acabou de entrar.`});
+                break;
+            case 'sendPublic':
+                await sendToAll(Object.keys(names),{publicMessage: `${names[connectionId]} : ${body.message}`});
+                break;
+            case 'sendBot':
+                await sendToAll(Object.keys(names),{publicMessage: `${names[connectionId]} : ${body.message}`});
+                await sendToBot(Object.keys(names), body.message);
+                break;
+            case 'sendPrivate':
+                const to  = Object.keys(names).find(key => names[key] === body.to);
+                await sendToOne(to,{privateMessage:  `${names[connectionId]} : ${body.message}`});
+                break;
+            default:
+        }
+    }
+    
+    
+    
+    // TODO implement
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify('Hello from Lambda!'),
+    };
+    return response;
+};
